4cb58a4ffb5fd82d15aaf95784df99c57038d30f
MutisoMuli <mulimutiso525@gmail.com>
2023-12-13 14:35:53 +0300

    Done

--- functs1.c
+++ functs1.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_unsigned - Prints the allocated unsigned #
- * @types: List a of args in the function
+ * print_unsigned - Outputs the allocated unsigned #
+ * @types: List a of arguements in the function
  * @buffer: Buffer array to deal with print function
  * @flags:  Enumerates active flags
  * @width: secures  width in the function
  * @precision: The precision specs
  * @size: Size specs-fier
- * Return: The # of chars outputed
+ * Return: The # of characters outputed
  */
 int print_unsigned(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
@@ -33,15 +33,15 @@ int print_unsigned(va_list types, char buffer[],
 }
 
 /**
- * output_octal - Outputs an unsigned # in octal notation
- * @types: Lists of args
+ * output_octal - Prints an unsigned # in octal notation
+ * @types: Lists of arguements
  * @buffer: Buffer array to handle print funct in the code
  * block
  * @flags:  Computes active flags
  * @width: Aquires the width of a funct
  * @precision: Precision specs-fication
  * @size: Size specifier in the funct
- * Return: # of chars outputed
+ * Return: # of characters outputed
  */
 int output_octal(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
@@ -71,14 +71,14 @@ int output_octal(va_list types, char buffer[],
 	return (output_unsigned(0, i, buffer, flags, width, precision, size));
 }
 /**
- * output_hexadecimal - Outputs an unsigned # in hexadecimal notation
- * @types: Lists of args
+ * output_hexadecimal - Prints an unsigned # in hexadecimal notation
+ * @types: Lists of arguements
  * @buffer: Buffer array to handle print functions
  * @flags:  Calculates active flags
  * @width: Enumerates width
  * @precision: Disntinctness specification
  * @size: Size specs-fier
- * Return: # of chars outputed
+ * Return: # of characters outputed
  */
 int output_hexadecimal(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
@@ -87,14 +87,14 @@ int output_hexadecimal(va_list types, char buffer[],
 				flags, 'x', width, precision, size));
 }
 /**
- * output_upper_hexa - Prints an unsigned number in upper hexadecimal notation
+ * output_upper_hexa - Outputs an unsigned number in upper hexadecimal notation
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed
+ * Return: Number of characters printed
  */
 int output_upper_hexa(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
@@ -103,8 +103,8 @@ int output_upper_hexa(va_list types, char buffer[],
 				flags, 'X', width, precision, size));
 }
 /**
- * output_hexas - Outputs a hexadecimal #s in lower or upper
- * @types: Lists  args
+ * output_hexas - Prints a hexadecimal #s in lower or upper
+ * @types: Lists  arguements
  * @map_to: Array of values to map the # to
  * @buffer: Buffer array to handle print functs
  * @flags:  Computes active flags
@@ -113,7 +113,7 @@ int output_upper_hexa(va_list types, char buffer[],
  * @precision: Distinctiveness specs
  * @size: Size specs-fiers
  * @size: The # size specs
- * Return:  of chars printed
+ * Return:  of characters printed
  */
 int output_hexas(va_list types, char map_to[], char buffer[],
 			int flags, char flag_ch, int width, int precision, int size)
